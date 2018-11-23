3e5a3006e0ba04e32e27911ceba8190940d28133
Walid Sultan <walidalysultan@gmail.com>
2018-11-22 19:05:46 -0800

    Added fading opacity animation for the difficulty menu

--- src/scripts/components/LevelDifficulty.tsx
+++ src/scripts/components/LevelDifficulty.tsx
@@ -2,34 +2,58 @@
 import * as React from 'react';
 import LevelDifficultyInterface from '../interfaces/LevelDiffiCultyInterface';
 import Popup from './popup';
-import { View, TouchableHighlight, Text } from 'react-native';
+import { View, TouchableHighlight, Text, Animated } from 'react-native';
 import LevelDifficultyStyles from '../../styles/levelDifficultyStyles';
+import LevelDifficultyState from '../states/LevelDifficultyState';
 
-export default class LevelDifficulty extends React.Component<LevelDifficultyInterface, {}> {
+export default class LevelDifficulty extends React.Component<LevelDifficultyInterface, LevelDifficultyState> {
 
     constructor(props: any) {
         super(props);
-
+        this.state = new LevelDifficultyState();
     }
     render() {
+
+        let easyButtonStyle = {
+            opacity: this.state.EasyButtonOpacity
+        }
+
+        let mediumButtonStyle = {
+            opacity: this.state.MediumButtonOpacity
+        }
+        let hardButtonStyle = {
+            opacity: this.state.HardButtonOpacity
+        }
         return (
             <Popup showPopup={this.props.showPopup}
                 title={this.props.title}
                 onCloseClick={() => this.props.onCloseClick()}
             >
                 <View style={LevelDifficultyStyles.container}>
-                    <TouchableHighlight onPress={() => { this.props.onEasyLevelClick(); }} style={LevelDifficultyStyles.button} underlayColor='#eee'>
-                        <Text style={LevelDifficultyStyles.buttonText}>Easy</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => { this.props.onMediumLevelClick(); }} style={LevelDifficultyStyles.button} underlayColor='#eee'>
-                        <Text style={LevelDifficultyStyles.buttonText}>Meduim</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => { this.props.onHardLevelClick(); }} style={LevelDifficultyStyles.button} underlayColor='#eee'>
-                        <Text style={LevelDifficultyStyles.buttonText}>Hard</Text>
-                    </TouchableHighlight>
+                    <Animated.View style={[LevelDifficultyStyles.button, easyButtonStyle]}>
+                        <TouchableHighlight onPress={() => this.buttonPress(this.state.EasyButtonOpacity, this.props.onEasyLevelClick)} style={LevelDifficultyStyles.buttonHighlight} underlayColor='#eee'>
+                            <Text style={LevelDifficultyStyles.buttonText}>Easy</Text>
+                        </TouchableHighlight>
+                    </Animated.View>
+                    <Animated.View style={[LevelDifficultyStyles.button, mediumButtonStyle]}>
+                        <TouchableHighlight onPress={() => this.buttonPress(this.state.MediumButtonOpacity, this.props.onMediumLevelClick)} style={LevelDifficultyStyles.buttonHighlight} underlayColor='#eee'>
+                            <Text style={LevelDifficultyStyles.buttonText}>Meduim</Text>
+                        </TouchableHighlight>
+                    </Animated.View>
+                    <Animated.View style={[LevelDifficultyStyles.button, hardButtonStyle]}>
+                        <TouchableHighlight onPress={() => this.buttonPress(this.state.HardButtonOpacity, this.props.onHardLevelClick)} style={LevelDifficultyStyles.buttonHighlight} underlayColor='#eee'>
+                            <Text style={LevelDifficultyStyles.buttonText}>Hard</Text>
+                        </TouchableHighlight>
+                    </Animated.View>
                 </View>
             </Popup>
         );
+    }
+
+    buttonPress(animatedValue: Animated.Value, callback: any) {
+        Animated.timing(animatedValue, { toValue: 0.1, duration: 500 }).start(() => {
+            callback();
+        });
     }
 
 }
---
+++ src/scripts/states/LevelDifficultyState.ts
@@ -0,0 +1,12 @@
+import { Animated } from "react-native";
+
+export default class LevelDifficultyState {
+    constructor(){
+        this.EasyButtonOpacity= new Animated.Value(1);
+        this.MediumButtonOpacity= new Animated.Value(1);
+        this.HardButtonOpacity= new Animated.Value(1);
+    }
+    EasyButtonOpacity:Animated.Value;
+    MediumButtonOpacity:Animated.Value;
+    HardButtonOpacity:Animated.Value;
+}
